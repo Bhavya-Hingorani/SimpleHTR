d4167287cfe3a28858db0c9c57044f31668b4397
unknown <bhavyahingorani54@gmail.com>
2022-03-29 09:13:39 +0530

    Write to file functionality added

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 from typing import Tuple, List
-
+import sys
 import cv2
 import editdistance
 from path import Path
@@ -13,10 +13,9 @@ from preprocessor import Preprocessor
 
 class FilePaths:
     """Filenames and paths to data."""
-    fn_char_list = '../model/charList.txt'
-    fn_summary = '../model/summary.json'
-    fn_corpus = '../data/corpus.txt'
-
+    fn_char_list = 'D:/project/SimpleHTR/model/charList.txt'
+    fn_summary = 'D:/project/SimpleHTR/model/summary.json'
+    fn_corpus = 'D:/project/SimpleHTR/data/corpus.txt'
 
 def get_img_height() -> int:
     """Fixed height for NN."""
@@ -26,8 +25,8 @@ def get_img_height() -> int:
 def get_img_size(line_mode: bool = False) -> Tuple[int, int]:
     """Height is fixed for NN, width is set according to training mode (single words or text lines)."""
     if line_mode:
-        return 256, get_img_height()
-    return 128, get_img_height()
+        return 800, get_img_height()
+    return 512, get_img_height()
 
 
 def write_summary(char_error_rates: List[float], word_accuracies: List[float]) -> None:
@@ -55,17 +54,17 @@ def train(model: Model,
     # stop training after this number of epochs without improvement
     while True:
         epoch += 1
-        print('Epoch:', epoch)
+        # print('Epoch:', epoch)
 
         # train
-        print('Train NN')
+        # print('Train NN')
         loader.train_set()
         while loader.has_next():
-            iter_info = loader.get_iterator_info()
+            iter_info = loader.get_iterator_info()  
             batch = loader.get_next()
             batch = preprocessor.process_batch(batch)
             loss = model.train_batch(batch)
-            print(f'Epoch: {epoch} Batch: {iter_info[0]}/{iter_info[1]} Loss: {loss}')
+            # print(f'Epoch: {epoch} Batch: {iter_info[0]}/{iter_info[1]} Loss: {loss}')
 
         # validate
         char_error_rate, word_accuracy = validate(model, loader, line_mode)
@@ -77,23 +76,23 @@ def train(model: Model,
 
         # if best validation accuracy so far, save model parameters
         if char_error_rate < best_char_error_rate:
-            print('Character error rate improved, save model')
+            # print('Character error rate improved, save model')
             best_char_error_rate = char_error_rate
             no_improvement_since = 0
             model.save()
         else:
-            print(f'Character error rate not improved, best so far: {char_error_rate * 100.0}%')
+            # print(f'Character error rate not improved, best so far: {char_error_rate * 100.0}%')
             no_improvement_since += 1
 
         # stop training if no more improvement in the last x epochs
         if no_improvement_since >= early_stopping:
-            print(f'No more improvement since {early_stopping} epochs. Training stopped.')
+            # print(f'No more improvement since {early_stopping} epochs. Training stopped.')
             break
 
 
 def validate(model: Model, loader: DataLoaderIAM, line_mode: bool) -> Tuple[float, float]:
     """Validates NN."""
-    print('Validate NN')
+    # print('Validate NN')
     loader.validation_set()
     preprocessor = Preprocessor(get_img_size(line_mode), line_mode=line_mode)
     num_char_err = 0
@@ -102,25 +101,25 @@ def validate(model: Model, loader: DataLoaderIAM, line_mode: bool) -> Tuple[floa
     num_word_total = 0
     while loader.has_next():
         iter_info = loader.get_iterator_info()
-        print(f'Batch: {iter_info[0]} / {iter_info[1]}')
+        # print(f'Batch: {iter_info[0]} / {iter_info[1]}')
         batch = loader.get_next()
         batch = preprocessor.process_batch(batch)
         recognized, _ = model.infer_batch(batch)
 
-        print('Ground truth -> Recognized')
+        # print('Ground truth -> Recognized')
         for i in range(len(recognized)):
             num_word_ok += 1 if batch.gt_texts[i] == recognized[i] else 0
             num_word_total += 1
             dist = editdistance.eval(recognized[i], batch.gt_texts[i])
             num_char_err += dist
             num_char_total += len(batch.gt_texts[i])
-            print('[OK]' if dist == 0 else '[ERR:%d]' % dist, '"' + batch.gt_texts[i] + '"', '->',
-                  '"' + recognized[i] + '"')
+            # print('[OK]' if dist == 0 else '[ERR:%d]' % dist, '"' + batch.gt_texts[i] + '"', '->',
+            #       '"' + recognized[i] + '"')
 
     # print validation result
     char_error_rate = num_char_err / num_char_total
     word_accuracy = num_word_ok / num_word_total
-    print(f'Character error rate: {char_error_rate * 100.0}%. Word accuracy: {word_accuracy * 100.0}%.')
+    # print(f'Character error rate: {char_error_rate * 100.0}%. Word accuracy: {word_accuracy * 100.0}%.')
     return char_error_rate, word_accuracy
 
 
@@ -134,6 +133,9 @@ def infer(model: Model, fn_img: Path) -> None:
 
     batch = Batch([img], None, 1)
     recognized, probability = model.infer_batch(batch, True)
+    f = open("D:/project/WordDetector/Output/output.txt", "a")
+    f.write("{} ".format(recognized[0]))
+    f.close()
     print(f'Recognized: "{recognized[0]}"')
     print(f'Probability: {probability[0]}')
 
@@ -148,7 +150,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--data_dir', help='Directory containing IAM dataset.', type=Path, required=False)
     parser.add_argument('--fast', help='Load samples from LMDB.', action='store_true')
     parser.add_argument('--line_mode', help='Train to read text lines instead of single words.', action='store_true')
-    parser.add_argument('--img_file', help='Image used for inference.', type=Path, default='../data/word.png')
+    parser.add_argument('--img_file', help='Image used for inference.', type=Path, default='D:/project/SimpleHTR/data/custom_2.png')
     parser.add_argument('--early_stopping', help='Early stopping epochs.', type=int, default=25)
     parser.add_argument('--dump', help='Dump output of NN to CSV file(s).', action='store_true')
 
